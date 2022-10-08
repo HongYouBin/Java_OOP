f95fb6230e33d658e880a478e3fd349daa83d925
HongYouBin <ghddbqls33@gmail.com>
2022-10-08 19:20:29 +0900

    spring container & spring bean

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -20,7 +20,7 @@ public class OrderServiceTest {
     @BeforeEach
     public void beforeEach(){
         AppConfig appConfig = new AppConfig();
-        memberService = appConfig.MemberService();
+        memberService = appConfig.memberService();
         orderService = appConfig.orderService();
     }
 
