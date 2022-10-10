a61478a58f2cb7fe2517c31c939f3ec816950b92
HongYouBin <ghddbqls33@gmail.com>
2022-10-10 21:57:52 +0900

    componenet scan

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        //basePackages = "hello.core.member", //시작할 패키지 위치 정한다.
+        //basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,16 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+//@Component("memberService2")
+@Component()
 public class MemberServiceImpl implements  MemberService{
     //추상화에도 의존(MemberRepository) 구체화에도 의존(new MemoryMemberRepository()), dip 위반
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
     private  final MemberRepository memberRepository;
+
+    @Autowired //ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
